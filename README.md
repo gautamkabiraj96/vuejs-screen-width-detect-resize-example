e6bee043306a91dfa7c3452c5c9c357d06f63c9f
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-11-03 01:22:16 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vuejs-screen-width-detect-resize-example
-Created with CodeSandbox
+#How to detect screen width and update it on resize in Vue.js
+Learn how to detect the current screen width of the browser or the app in Vue.js and update it's value in a reactive state on resizing the window.
+[Learn more](https://www.nightprogrammer.com/vue-js/how-to-detect-screen-width-and-update-it-on-resize-in-vue-js-example/)
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <p>Screen width: {{ screenWidth }}px</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      screenWidth: 0,
+    };
+  },
+  mounted() {
+    this.updateScreenWidth();
+    this.onScreenResize();
+  },
+  methods: {
+    onScreenResize() {
+      window.addEventListener("resize", () => {
+        this.updateScreenWidth();
+      });
+    },
+    updateScreenWidth() {
+      this.screenWidth = window.innerWidth;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
